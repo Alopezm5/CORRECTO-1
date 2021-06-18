3d29d0c335d1ab4549aaa7815c3d28ec319a8ddb
Alopezm5 <85761855+Alopezm5@users.noreply.github.com>
2021-06-17 23:46:47 -0500

    Update 14*17-6-21.py
    Implementos de ciclos con saltos tanto en aumento como en decremento

--- 14*17-6-21.py
+++ 14*17-6-21.py
@@ -8,7 +8,16 @@ class For:
         numeross=(2,5.6,4,1)
         docente={"nombre":"Ashley", "edad":21,"fac":"facil"}
         listanotas=[(30,40),(20,40),(50,40)]
-
+        #range([inicio=0],limite,[incremento/decremento]. Genera un rang de valores desde un valor inicial a un valor dinal
+        #se ejecuta desde inicio hasta el limite
+        for i in range(5):
+            print("i={}".format(i))#ciclo de uno en uno
+        for i in range(2,10):
+            print("i={}".format(i))#ciclo de un valor inicial a un valor final
+        for i in range(4,10,2):
+            print("i={}".format(i),end=" ")#ciclo con valor inicial y valor final, y con parametros de saltos en aumento
+        for i in range(12,3,-3):
+            print("i={}".format(i))#ciclo con valor inicial y valor final, y con parametros de saltos de decremento
         
         
 objFor=For()
