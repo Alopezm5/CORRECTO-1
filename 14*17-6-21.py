5a4ba96668b7bef9b57271b56c76f6be442e4130
Alopezm5 <85761855+Alopezm5@users.noreply.github.com>
2021-06-17 23:36:42 -0500

    Create 14*17-6-21.py
    Asignacion de clase y metodo constructor

---
+++ 14*17-6-21.py
@@ -0,0 +1,15 @@
+class For:
+    def __init__(self):
+        pass
+
+    #ciclo repetitivo de incrementos o decrementos se ejecuta por verdadero
+    def usoFor(self):
+        datos=["Ashley",21,True]
+        numeross=(2,5.6,4,1)
+        docente={"nombre":"Ashley", "edad":21,"fac":"facil"}
+        listanotas=[(30,40),(20,40),(50,40)]
+
+        
+        
+objFor=For()
+objFor.usoFor()
